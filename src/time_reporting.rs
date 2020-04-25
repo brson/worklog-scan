8c250328c2ab077de6f34843b0d83d0e5edf7d4d
Brian Anderson <andersrb@gmail.com>
2020-04-25 17:02:35 -0600

    .

--- src/time_reporting.rs
+++ src/time_reporting.rs
@@ -144,11 +144,10 @@ fn print_report(start: NaiveDate, end: NaiveDate,
 
     println!("{}", STYLE);
     println!("");
-    println!("# Invoice for Brian Anderson re PingCAP");
+    println!("# Invoice for Brian Anderson re MobileCoin");
     println!();
     println!("name: Brian Anderson  ");
-    println!("email: andersrb@gmail.com / brian@pingcap.com  ");
-    println!("manager: Kevin Xu <kevin@pingcap.com>  ");
+    println!("email: andersrb@gmail.com");
     println!("reporting period: {} - {}  ", start, end);
     println!("total hours: {:.1}  ", total_hours);
     println!("hourly rate: ${:}  ", hourly_rate);
